ebbd29f0f073f4d23e25ac8781ff4f0a345dc25a
Andre <amelco.herman@gmail.com>
2017-01-12 18:43:38 -0200

    Add generation of a random population. Need to solve problem with creation of array of strings

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ An example of genetic algorithm
 
 The purpose of this program is to get familiarized with genetic algorithm.
 
-The program simply try to find the phras "To be or not to be." using genetic algorthim. This example is based on the Daniel Shiffman video on youtube (https://www.youtube.com/watch?v=9zfeTw-uFCw), which is based on his book "The nature of code" (http://natureofcode.com/book/).
+The program simply try to find the phrase "To be or not to be." using genetic algorthim. This example is based on the Daniel Shiffman video on youtube (https://www.youtube.com/watch?v=9zfeTw-uFCw), which is based on the chapter 9 from his book "The nature of code" (http://natureofcode.com/book/).
 
 He develops the genetic algorithm using java and javascript languages. This program will be entirelly written in FORTRAN 90.
---
+++ main.f90
@@ -0,0 +1,36 @@
+program genAlg
+implicit none
+
+! Declaration of variables
+character(100) :: phrase
+integer :: length
+integer :: seed
+integer :: i,j
+real :: rDNA(19)
+integer :: iDNA(19)
+!character(len=19), dimension(20) :: sDNA    ! Array of 20 strings with length 19
+character*19 sDNA(20)    ! Array of 20 strings with length 19
+
+
+! Variables initialization
+phrase = "To be or not to be."
+seed = 12345
+length = len(trim(phrase))
+
+
+! Firstly, we need to create a population of random generated phrases
+!  with the same length of the original one (ASCII code 32 to 125)
+! We will call each element of the population DNA
+
+call random_number(rDNA)
+
+! 19 is the length. Change to allocatable arrays later
+do j=1,20
+  iDNA = 32 + floor((125+1-32)*rDNA)
+  do i=1,19
+    sDNA(j,i:i) = char(iDNA(i))
+  enddo
+print*, sDNA(j)
+enddo
+
+end program
