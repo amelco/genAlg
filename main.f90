2d630bf8fabafc00520ff947a9bf8d921397edd4
Andre <amelco.herman@gmail.com>
2017-01-13 18:10:42 -0200

    Problem with array of strings solved. Need to find a true random number generator

--- main.f90
+++ main.f90
@@ -8,8 +8,7 @@ integer :: seed
 integer :: i,j
 real :: rDNA(19)
 integer :: iDNA(19)
-!character(len=19), dimension(20) :: sDNA    ! Array of 20 strings with length 19
-character*19 sDNA(20)    ! Array of 20 strings with length 19
+character(19), dimension(20) :: sDNA    ! Array of 20 strings with length 19
 
 
 ! Variables initialization
@@ -26,11 +25,15 @@ call random_number(rDNA)
 
 ! 19 is the length. Change to allocatable arrays later
 do j=1,20
-  iDNA = 32 + floor((125+1-32)*rDNA)
+iDNA = 32 + floor((125+1-32)*rDNA)
   do i=1,19
-    sDNA(j,i:i) = char(iDNA(i))
+    if (i==1) then
+      sDNA(j) = char(iDNA(i))
+    else
+      sDNA(j) = trim(sDNA(j)) // char(iDNA(i))
+    endif
   enddo
-print*, sDNA(j)
+  print*, sDNA(j)
 enddo
 
 end program
