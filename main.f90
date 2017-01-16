63e6bda365dc9e8a6d927fcd4f9ac51b453079c4
Andre <amelco.herman@gmail.com>
2017-01-16 10:24:59 -0200

    Finished\!

--- main.f90
+++ main.f90
@@ -6,27 +6,40 @@ character(100) :: phrase
 integer :: length
 real :: seed
 integer :: pop_size
+real :: mutation_rate
 integer :: i,j,k
-real :: rDNA(19)
-integer :: iDNA(19)
-character(19), dimension(20) :: sDNA    ! Array of 20 strings with length 19
-integer, dimension(20) :: fitness       ! It must have the same size of population
-real, dimension(20) :: norm_fit         ! It must have the same size of population
-integer, dimension(100) :: pool         ! Mating pool with size 100 to represent 100%
+real, allocatable :: rDNA(:)
+integer, allocatable :: iDNA(:)
+character(:), dimension(:), allocatable :: sDNA    ! Array of 20 strings with length 19
+integer :: score
+real, dimension(:), allocatable :: fitness       ! It must have the same size of population
+real, dimension(:), allocatable :: norm_fit         ! It must have the same size of population
+integer, dimension(:), allocatable :: pool         ! Mating pool with size 100 to represent 100%
 integer :: parentA, parentB
 character(100) :: child
 integer :: generation
 
 
 ! Variables initialization
-phrase = "To be or not to be."
-pop_size = 20
+phrase = "To be or not to be. That is the question!"
+pop_size = 100
+mutation_rate = 0.002
 seed = 12345.6
 length = len(trim(phrase))
-fitness = 0
+fitness = 0.0
 pool = 0
 generation = 1
+score = 0
 
+allocate(character(length) :: sDNA(pop_size))
+allocate(fitness(pop_size))
+allocate(norm_fit(pop_size))
+allocate(pool(pop_size))
+allocate(rDNA(length))
+allocate(iDNA(length))
+
+! generates different random numbers at each execution
+call random_seed()
 
 !!!!!!!!!!!!!!!!! Creation of random population !!!!!!!!!!!!!!!!!!!!!!!!!
 ! Firstly, we need to create a population of random generated phrases
@@ -50,30 +63,31 @@ enddo
 
 !!! HERE THE LOOP BEGINS
 do
-  ! Verify if, from the previous step, there is a phrase with fitness 1 (100% correct)
-  do j=1,pop_size
-    if (norm_fit(j) == 1.0) then
-      exit
-    endif
-  enddo
-
   ! SHOW ON SCREEN
   !call system('clear')
   !print*, generation, sDNA(maxloc(fitness)), maxval(fitness)
 
 
-  !!!!!!!!!!!!!!!!! Selction of the fittest !!!!!!!!!!!!!!!!!!!!!!!!!
+  !!!!!!!!!!!!!!!!! Selection of the fittest !!!!!!!!!!!!!!!!!!!!!!!!!
   ! Now we have to calculate the fitness of each element of the population (DNA)
   ! It is generally a mathematical function, but in this case it will be
   !  the number of correct characters that are in the correct position.
   
   do j=1,pop_size
+    score = 0
     do i=1,length
       if (sDNA(j)(i:i) .eq. phrase(i:i)) then
-        fitness(j) = fitness(j) + 1
+        score = score + 1
       endif
     enddo
-    print*, j, fitness(j)
+    fitness(j) = (1.0*score)/(1.0*length)
+    !print*, j, fitness(j), score, length
+    if (fitness(j) == 1.0 .or. trim(sDNA(j)) == trim(phrase)) then
+      print*
+      print*, generation, j, sDNA(j), fitness(j)
+      print*
+      stop 
+    endif
   enddo
   
   !!! Choosing the parents !!!
@@ -86,10 +100,12 @@ do
   k=1
   i=1
   do j=1,pop_size
-    norm_fit(j) = real(fitness(j))/sum(fitness)
+    norm_fit(j) = fitness(j)/sum(fitness)
+!    print*, j, norm_fit(j), floor(norm_fit(j)*100)
     if (norm_fit(j) .ne. 0.0) then
-      !print*, j, norm_fit(j)
+!    if (fitness(j) .ne. 0.0) then
       do while (k <= floor(norm_fit(j)*100))
+!      do while (k <= floor(fitness(j)*100))
         pool(i) = j
         k = k+1
         i = i+1
@@ -119,33 +135,47 @@ do
     parentA = floor(seed*100)+1
     call random_number(seed)
     parentB = floor(seed*100)+1
-    !print*, parentA, parentB
     !print*, j,parentA, pool(parentA), sDNA(pool(parentA))
     !print*, j,parentB, pool(parentB), sDNA(pool(parentB))
+    !print*, j, parentA, sDNA(pool(parentA)), fitness(pool(parentA))
+    !print*, j, parentB, sDNA(pool(parentB)), fitness(pool(parentB))
     
     ! Reproducing
     child = sDNA(pool(parentA))(1:length/2) // sDNA(pool(parentB))(length/2+1:length)
-    !print*, j,child
+    !print*, trim(child)
   
     ! Mutating
     do i=1,length
       call random_number(seed)
-      if (seed < 0.01) then
+      if (seed < mutation_rate) then
         call random_number(seed)
         child(i:i) = char(32 + floor((125+1-32)*seed))
+        !print*, 'mutation'
       endif
     enddo
   
     ! Adding the child to the population
-    sDNA(j) = child
+    sDNA(j) = trim(child)
   
     ! printing new population
-    !print*, j, sDNA(j)
+    !print*, j, sDNA(j), fitness(j)
   enddo
 
+  print*, sDNA(maxloc(fitness)), maxval(fitness)
+
   generation = generation + 1
-  fitness = 0
- 
+
+  ! Verify if there is a phrase with fitness 1 (100% correct)
+!  do j=1,pop_size
+!    if (fitness(j) == 1.0) then
+!      print*
+!      print*, generation, j, sDNA(j)
+!      print*
+!      stop 
+!    endif
+!  enddo
+
+  fitness = 0.0
 enddo  ! END OF MAIN LOOP
 
 end program
